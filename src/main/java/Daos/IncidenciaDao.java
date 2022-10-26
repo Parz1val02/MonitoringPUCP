c7cb0ec1b976f0bb246caa49cb0b70f5c785280a
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2022-10-26 12:48:39 -0500

    Add files via upload

--- src/main/java/Daos/IncidenciaDao.java
+++ src/main/java/Daos/IncidenciaDao.java
@@ -1,6 +1,7 @@
 package Daos;
 
 import Beans.Incidencia;
+import com.mysql.cj.x.protobuf.MysqlxExpr;
 //import sun.nio.ch.WindowsAsynchronousFileChannelImpl;
 
 import java.sql.*;
@@ -99,5 +100,61 @@ public class IncidenciaDao {
         }
 
         return incidencia;
+    }
+    public ArrayList<Incidencia> obtenerDestacadas() throws SQLException {
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/telesystem_aa";
+
+        ArrayList<Incidencia> listaDestacadas = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, user, pass);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("select *, concat(u.nombre,\" \",u.apellido) as `Usuario`\n" +
+                     "from incidencia i \n" +
+                     "inner join nivelurgencia nu on nu.idNivelUrgencia = i.idNivelUrgencia\n" +
+                     "inner join tipoincidencia ti on ti.idTipoIncidencia = i.idTipoIncidencia\n" +
+                     "inner join usuarios u on i.codigousuario = u.codigo\n")
+        ) {
+
+            //Usuario usuario = new Usuario();
+            //Integer min_cont = 0;
+            //Integer[4] d= {0,0,0,0,0};
+            Integer[] d = new int[4];
+            Incidencia[4] listaDestacadas = new Incidencia();
+            while (rs.next())
+                Incidencia incidencia = new Incidencia();
+                incidencia.setIdIncidencia(rs.getInt(1));
+                incidencia.setFecha(rs.getString(2));
+                incidencia.setNombreIncidencia(rs.getString(3));
+                incidencia.setZonaPUCP(rs.getString(4));
+                incidencia.setLatitud(rs.getDouble(5));
+                incidencia.setLongitud(rs.getDouble(6));
+                incidencia.setValidaIncidencia(rs.getBoolean("validaIncidencia"));
+                incidencia.setDescripcion(rs.getString(8));
+                incidencia.setContador_reabierto(rs.getInt(9));
+                incidencia.setTipoIncidencia(rs.getString("tipo"));
+                incidencia.setNivelUrgencia(rs.getString("nivel"));
+                incidencia.setNombreEstado(rs.getString(12));
+                incidencia.setUsuarios_nombre_completo(rs.getString("Usuario"));
+                incidencia.setContadorDestacado(rs.getInt(14));
+                //incidencia.setUsuario(usuario);
+                for(int i=0; i<=4;i++){
+                    if (incidencia.getContadorDestacado()>= d[i]) {
+                        d[i] =incidecia.getContadorDestacado();
+                        listaDestacadas[i] = incidencia;
+                        break;
+                    }
+                }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listaDestacadas;
     }
 }
