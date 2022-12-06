c1582c0e98ab29711983a1fd5dce7f10a4a1facb
DiegoLG15 <a20200839@pucp.edu.pe>
2022-12-06 01:15:50 -0500

    completo PrimeraContrasenia

--- src/main/java/Servlets/Login.java
+++ src/main/java/Servlets/Login.java
@@ -1,5 +1,6 @@
 package Servlets;
 
+import Beans.FotoPerfil;
 import Beans.Incidencia;
 import Beans.MasterTable;
 import Beans.Usuario;
@@ -11,7 +12,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -70,12 +73,8 @@ public class Login extends HttpServlet {
                 response.sendRedirect(request.getContextPath() + "/Login");
                 break;
             case("firstpassword"):
-                view = request.getRequestDispatcher("/Login/ContraseniaPredeterminda.jsp");
+                view = request.getRequestDispatcher("/Login/ContraseniaPredeterminada.jsp");
                 view.forward(request, response);
-                break;
-            case("firstPass"):
-
-
                 break;
             case("2fa"):
                 view = request.getRequestDispatcher("/Login/doblefactor.jsp");
@@ -307,26 +306,87 @@ public class Login extends HttpServlet {
 
                 boolean usuarioMasterterTable = uDao.consultarMasterTable(codigo,mail);
                 if(usuarioMasterterTable){
-                    Usuario usuario1 = new Usuario();
+                    Usuario usuarioMaster = uDao.buscarPorIdMasterTable(codigo);
+                    if(uDao.validarPrimerIngreso(usuarioMaster)){
+                        String relativeWebPath = "./images/usuario.png";
+                        String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
+                        File file = new File(absoluteDiskPath);
+                        byte[] fileContent = Files.readAllBytes(file.toPath());
+
+                        FotoPerfil fp = new FotoPerfil();
+                        fp.setFotobyte(fileContent);
+                        fp.setNombreFoto("usuario.png");
+
+                        usuarioMaster.setFotoPerfil(fp);
+
+                        session.setAttribute("usuario",usuarioMaster);
+
+                        GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
+                        String firstPassword = generadorDeContrasenha.crearPassword();
+
+                        uDao.registroDesdeMastertable(usuarioMaster,firstPassword);
+
+                        try {
+                            EnviarPrimeraContrasenia.main(usuarioMaster.getCorreo(), firstPassword);
+                        } catch (MessagingException e) {
+                            e.printStackTrace();
+                        }
+                        session.setAttribute("msg","La contraseña de su nueva cuenta ha sido enviada a su correo");
 
 
-                    GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
-                    String firstPassword = generadorDeContrasenha.crearPassword();
-                    try {
-                        EnviarPrimeraContrasenia.main(username, Integer.parseInt(firstPassword));
-                    } catch (MessagingException e) {
-                        e.printStackTrace();
+                    /*datos de mastertble al usuario
+                    y le hago un insert de la contrasenia*/
+
+                        /*uDao.cambiarContrasenaUsuario(usuario1.getCorreo(), firstPassword);*/
+
+                        RequestDispatcher view = request.getRequestDispatcher("/Login/InicioSesion.jsp");
+                        view.forward(request, response);
+                        break;
+                    } else {
+                        session.setAttribute("msg","El usuario ya ha sido registrado");
+                        RequestDispatcher requestDispatcher= request.getRequestDispatcher("/Login/InicioSesion.jsp");
+                        requestDispatcher.forward(request,response);
+                        break;
+                    }
+                }else {
+                    session.setAttribute("msg","No es miembro de la comunidad estudiantil PUCP");
+                    RequestDispatcher requestDispatcher= request.getRequestDispatcher("/Login/InicioSesion.jsp");
+                    requestDispatcher.forward(request,response);
+                    break;
+                }
+            case("compararPrimeraContrasenia"):
+                Usuario usuario2 = (Usuario) session.getAttribute("usuario");
+                String contraseniaActual =request.getParameter("contraseñaActual");
+                String contraseniaNueva = request.getParameter("contraseñaNueva");
+                String confirmContrasenia =request.getParameter("repass");
+
+                if(uDao.compararPrimeraContrasenia(contraseniaActual,usuario2)){
+                    if(contraseniaNueva.equals(confirmContrasenia)){
+                        uDao.cambiarContrasenaUsuario(usuario2.getCorreo(),contraseniaNueva);
+                        uDao.cambiarPrimerIngreso(usuario2);
+                        int flag = 1;
+                        session.setAttribute("flag",flag);
+                        response.sendRedirect(request.getContextPath() + "/UsuarioServlet");
+
+                        break;
+                    } else {
+                        session.setAttribute("msg","Las contraseñas ingresadas no coinciden");
+                        RequestDispatcher requestDispatcher= request.getRequestDispatcher("/Login/ContraseniaPredeterminada.jsp");
+                        requestDispatcher.forward(request,response);
+
+                        break;
                     }
 
+                } else {
+                    session.setAttribute("msg","La contraseña actual no es correcta");
+                    RequestDispatcher requestDispatcher= request.getRequestDispatcher("/Login/ContraseniaPredeterminada.jsp");
+                    requestDispatcher.forward(request,response);
+
+                    break;
                 }
 
-                int bandera=0;
 
 
-
-
-
-                break;
             default:
                 response.sendRedirect(request.getContextPath() + "/Login");
         }
