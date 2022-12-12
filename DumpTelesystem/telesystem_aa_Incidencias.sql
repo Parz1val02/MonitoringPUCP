9fa0826104b1173850df1aa498084bf374bdd4d7
r.b4rr10s <a20202073@pucp.edu.pe>
2022-12-11 19:15:46 -0500

    Funcion capitalilze y dump final para la base de datos

---
+++ DumpTelesystem/telesystem_aa_Incidencias.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE  IF NOT EXISTS `telesystem_aa` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
+USE `telesystem_aa`;
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: telesystem_aa
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Incidencias`
+--
+
+DROP TABLE IF EXISTS `Incidencias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Incidencias` (
+  `idIncidencia` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `nombreIncidencia` varchar(45) NOT NULL,
+  `validaIncidencia` tinyint(4) NOT NULL,
+  `descripcion` text NOT NULL,
+  `contadorReabierto` int(11) DEFAULT NULL,
+  `otroTipo` varchar(45) DEFAULT NULL,
+  `idTipoIncidencia` int(11) NOT NULL,
+  `idNivelUrgencia` int(11) NOT NULL,
+  `idEstadoIncidencia` int(11) NOT NULL,
+  `codigoUsuario` varchar(8) NOT NULL,
+  `idZonaPUCP` int(11) NOT NULL,
+  PRIMARY KEY (`idIncidencia`),
+  KEY `fk_Incidencia_TipoIncidencia1_idx` (`idTipoIncidencia`),
+  KEY `fk_Incidencia_NivelUrgencia1_idx` (`idNivelUrgencia`),
+  KEY `fk_Incidencia_table11_idx` (`idEstadoIncidencia`),
+  KEY `fk_Incidencia_Usuarios1_idx` (`codigoUsuario`),
+  KEY `fk_Incidencias_ZonaPUCP1_idx` (`idZonaPUCP`),
+  CONSTRAINT `fk_Incidencia_NivelUrgencia1` FOREIGN KEY (`idNivelUrgencia`) REFERENCES `NivelUrgencia` (`idNivelUrgencia`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Incidencia_TipoIncidencia1` FOREIGN KEY (`idTipoIncidencia`) REFERENCES `TipoIncidencia` (`idTipoIncidencia`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Incidencia_Usuarios1` FOREIGN KEY (`codigoUsuario`) REFERENCES `Usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Incidencia_table11` FOREIGN KEY (`idEstadoIncidencia`) REFERENCES `EstadoIncidencia` (`idEstadoIncidencia`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Incidencias_ZonaPUCP1` FOREIGN KEY (`idZonaPUCP`) REFERENCES `ZonaPUCP` (`idZonaPUCP`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Incidencias`
+--
+
+LOCK TABLES `Incidencias` WRITE;
+/*!40000 ALTER TABLE `Incidencias` DISABLE KEYS */;
+INSERT INTO `Incidencias` VALUES
+(1,'2022-12-09','Accidente de tránsito',1,'Camioneta se chocó y derribó un muro',0,NULL,1,3,1,'20202330',12),
+(2,'2022-12-11','Accidente en semana de Ingeniería',1,'Lesión de alumna en evento',0,NULL,1,2,1,'20202330',11),
+(3,'2022-12-09','Robo de bicicleta',1,'Se robaron mi bicicleta cerca de generales letras',0,NULL,2,2,1,'20202073',4),
+(4,'2022-12-11','Robo en comedor central',1,'Sujetos armados ingresaron y robaron',0,NULL,2,3,1,'20201696',7),
+(5,'2022-12-10','Problemas en la graduación',1,'No dejan entrar a las personas',0,'Alerta',6,1,1,'20171198',5);
+/*!40000 ALTER TABLE `Incidencias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-11 19:01:13
---
+++ DumpTelesystem/telesystem_aa_Usuarios.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE  IF NOT EXISTS `telesystem_aa` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
+USE `telesystem_aa`;
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: telesystem_aa
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Usuarios`
+--
+
+DROP TABLE IF EXISTS `Usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuarios` (
+  `codigo` varchar(8) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `DNI` varchar(8) NOT NULL,
+  `validaUsuario` tinyint(4) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `celular` varchar(9) DEFAULT NULL,
+  `idRoles` int(11) NOT NULL,
+  `idCategoriaPUCP` int(11) DEFAULT NULL,
+  `idFotoPerfil` int(11) DEFAULT NULL,
+  `codigo2fa` int(11) DEFAULT NULL,
+  `activeTime2fa` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `fk_Usuarios_Roles_idx` (`idRoles`),
+  KEY `fk_Usuarios_CategoriaPUCP1_idx` (`idCategoriaPUCP`),
+  KEY `fk_Usuarios_FotoPerfil1_idx` (`idFotoPerfil`),
+  CONSTRAINT `fk_Usuarios_CategoriaPUCP1` FOREIGN KEY (`idCategoriaPUCP`) REFERENCES `CategoriaPUCP` (`idCategoriaPUCP`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Usuarios_FotoPerfil1` FOREIGN KEY (`idFotoPerfil`) REFERENCES `FotoPerfil` (`idFotoPerfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Usuarios_Roles` FOREIGN KEY (`idRoles`) REFERENCES `Roles` (`idRoles`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuarios`
+--
+
+LOCK TABLES `Usuarios` WRITE;
+/*!40000 ALTER TABLE `Usuarios` DISABLE KEYS */;
+INSERT INTO `Usuarios` VALUES
+('20113456','Goku','kakaroto','goku.kakaroto@pucp.edu.pe','56546767',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','976976976',2,NULL,NULL,NULL,NULL),
+('20171198','Luis','Ramos','luis.ramost@pucp.edu.pe','78349206',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','928374937',1,3,2,NULL,NULL),
+('20195922','Carlos','Pisco','cpisco@pucp.edu.pe','65358947',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','976239385',1,5,3,NULL,NULL),
+('20200334','Gil','Zanabria','a20200334@pucp.edu.pe','87654321',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a',NULL,1,4,4,NULL,NULL),
+('20200638','Alejandro','Gutierrez','a20200638@pucp.edu.pe','49543859',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','956031478',1,4,5,NULL,NULL),
+('20200839','Diego','Lavado','a20200839@pucp.edu.pe','41597284',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','94712498',1,1,6,NULL,NULL),
+('20201696','Leonardo','Abanto','a20201696@pucp.edu.pe','61485249',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','987654321',1,1,1,NULL,NULL),
+('20202073','Rodrigo','Barrios','a20202073@pucp.edu.pe','79134126',1,'8340e741b3af9bb3af606645085cb8a55ee97cda1802cb6107161ab8d238182d',NULL,1,1,7,NULL,NULL),
+('20202330','Dana','Nolasco','a20202330@pucp.edu.pe','61472198',1,'53e5a84fd38c5f0864994678f388beed42230498fbbefab983a1d27aad446f3a','923417981',1,2,8,NULL,NULL);
+/*!40000 ALTER TABLE `Usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-11 19:01:13
